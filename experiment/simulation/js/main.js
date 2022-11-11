e5fd532f5831a0a405cfc2a04e37afcc08533ba8
chandancmishra <chandan5108@gmail.com>
2022-11-11 17:20:27 +0530

    upload

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,1 +1,15 @@
 //Your JavaScript goes in here
+function  showDiv(str)
+{
+	if(str == '1')
+	{
+		document.getElementById("part1Div").style.display = '';
+		document.getElementById("part2Div").style.display = 'none';
+	}
+	else
+	{
+		document.getElementById("part2Div").style.display = '';
+		document.getElementById("part1Div").style.display = 'none';
+	}
+	
+}
